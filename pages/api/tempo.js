bb5bcf4f1170e60e5280904cf7539c6f1ab28c62
Ruben <rubenbsi.ti@gmail.com>
2021-04-29 21:22:37 -0300

    Exemplo de API rest com API Secret 2

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,7 +1,7 @@
 async function tempo(request, response){
     const apiSecret = process.env.CONVERTKIT_API_SECRET;
     const dynamicDate = new Date(); 
-
+ 
     const subscribersResponse = await fetch("https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}");
     const subscribersResponseJson = await subscribersResponse.json();
     const inscritos = subscribersResponseJson.total_subscribers;
